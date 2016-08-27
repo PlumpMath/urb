537d5afde54c41ddbd9b244ffc7be66ef5db1e2f
TheTrung <deulamco@gmail.com>
2016-08-27 17:45:43 +0700

    [Dirty Approach] transformed require, class, def into C# syntax

--- Urb/Program.cs
+++ Urb/Program.cs
@@ -1,57 +1,19 @@
 ﻿using System;
+using System.IO;
 
 namespace Urb
 {
 	class MainClass
 	{
-		public static void Main (string[] args)
+		public static void Main(string[] args)
 		{
-			var urb = new UrbCore ();
-			Console.WriteLine ("* Urb :: A Rubylike post-fix language compiler *");
+			var urb = new UrbCore();
+			Console.WriteLine("* Urb :: A Rubylike post-fix language compiler *");
 
 			// Test Source:
-			string test = @" 
-			  require System
-			  require UnityEngine
-			 
-			  class Player < MonoBehaviour
-			  
-			    Dictionary(String:String) new 
-				@dict pop
-				   
-				Stack(Object) new 
-				@stack pop
-				  
-				def test
-				  condition:
-				    1 i +=
-				    i Console.WriteLine
-				    10 i > if 
-				      :condition jump
-				    end
-				    i result =
-				  
-				  goodbye:
-				    ""Good bye"" Console.WriteLine
-				    result
-				end
+			string test = File.ReadAllText("../../examples/demo 2.urb");
+			urb.Parse(test, isDebug: false);
 
-				def set_position x:float y:float z:float
-				  transform.position.x x +
-				  transform.position.y y +
-				  transform.position.z z +
-				  Vector3 new
-				  transform.position pop
-				end
-				   
-				def Update
-				  KeyCode.DownArrow Input.GetKey if
-				    0f -0.1f 0f set_position
-				  end
-				end
-			  end
-";
-			urb.Parse (test, isDebug:false);
 			//Console.ReadLine ();
 		}
 	}
--- Urb/UrbCore.cs
+++ Urb/UrbCore.cs
@@ -8,29 +8,27 @@ namespace Urb
 	public class UrbCore
 	{
 		#region Collections
-		// Stack 
-		private Stack<object> mainStack = new Stack<object>();
-		// Line Stack
-		private Stack<object> lineStack = new Stack<object>();
 		// function
-		private Dictionary<string, Action<object>> functionMap = 
+		private Dictionary<string, Action<object>> functionMap =
 			new Dictionary<string, Action<object>>();
 		// codeblock
-		private Dictionary<string, Action> codeblockMap = 
+		private Dictionary<string, Action> codeblockMap =
 			new Dictionary<string, Action>();
 		#endregion
 
 		#region Init 
-		public UrbCore(){}
+		public UrbCore() { }
 
 		// Create new codeblock.
-		public void NewCodeBlock(string name, Action codeBlock){
-			codeblockMap.Add (name, codeBlock);
+		public void NewCodeBlock(string name, Action codeBlock)
+		{
+			codeblockMap.Add(name, codeBlock);
 		}
 
 		// Create new function.
-		public void NewFunction(string name, Action<object> codeBlock){
-			functionMap.Add (name, codeBlock);
+		public void NewFunction(string name, Action<object> codeBlock)
+		{
+			functionMap.Add(name, codeBlock);
 		}
 
 		#endregion
@@ -51,21 +49,22 @@ namespace Urb
 			@"(?<string>\"".*\"")|" +
 			// pair of a:b
 			@"(?<pair>[a-zA-Z0-9$_]+:[a-zA-Z0-9$_]+)|" +
-		
+
 			// @variable
 			@"(?<instance_variable>\@[a-zA-Z0-9$_]+)|" +
 
 			// float 1f 2.0f
-			@"(?<float>[-+]?[0-9]*\.?[0-9]+f)|"+
+			@"(?<float>[-+]?[0-9]*\.?[0-9]+f)|" +
 			// integer 120
 			@"(?<integer>[+-]?[0-9]+)|" +
 			// operators
 			@"(?<operator>[\+|-|\*|\/|\^|\+\=|\-\=])|" +
 			// boolean
-		    @"(?<boolean_operator>[\>|\<|\=|\==|\>=|\<=])|"+
+			@"(?<boolean_compare>[\>|\<|\=|\==|\>=|\<=])|" +
+			@"(?<boolean_condition>[\|\||\&\&])|" +
 
 			//special_form
-			@"(?<special_form>def|end|class|require|if)|" +
+			@"(?<special_form>def|end|class|require|if|new)|" +
 
 			// :Symbol
 			@"(?<symbol>:[a-zA-Z0-9$_]+)|" +
@@ -79,15 +78,18 @@ namespace Urb
 		#endregion
 
 		// Readline.
-		public void Parse(string source, bool isDebug = false){
-			var token_list = new List<Token> ();
-			var regex_pattern = new Regex (pattern);
-			var matches = regex_pattern.Matches (source);
+		public void Parse(string source, bool isDebug = false)
+		{
+			var token_list = new List<Token>();
+			var regex_pattern = new Regex(pattern);
+			var matches = regex_pattern.Matches(source);
 
-			foreach (Match match in matches){
+			foreach (Match match in matches)
+			{
 				int i = 0;
-				foreach (Group group in match.Groups){
-					
+				foreach (Group group in match.Groups)
+				{
+
 					var match_value = group.Value;
 
 					var success = group.Success;
@@ -101,9 +103,10 @@ namespace Urb
 							token_list.Add(new Token(group_name, match_value));
 
 							//if (groupName != "newline") 
-							if(isDebug) 
+							if (isDebug)
 								Console.WriteLine("{0} - {1}", group_name, match_value);
-						}}
+						}
+					}
 					i++;
 				}
 			}
@@ -111,111 +114,157 @@ namespace Urb
 			Lex(token_list);
 		}
 
-		private void Lex(List<Token> token_list){
+		private List<List<Token>> blocks = new List<List<Token>>();
+		private List<string> csharp_blocks = new List<string>();
+
+		private void InspectLine(List<Token> line)
+		{
+			foreach (var word in line) Console.Write("{0} ", word);
+			Console.WriteLine();
+		}
+
+		private void AddSource(string line)
+		{
+			csharp_blocks.Add(line);
+		}
+
+		private string current_special_form = String.Empty;
+
+		// Well, I think we should play dirty :P 
+		private void Lex(List<Token> token_list)
+		{
 			Console.WriteLine("Lexing..");
-			// Where we process tokens
-			var is_special = false;
-			var is_being_nested = false;
-			var is_new_line = false;
-			var nest_level = 0;
 			var acc = new List<Token>();
-			var line = new List<Token>();
 
 			var token_array = token_list.ToArray();
 			Console.WriteLine("Token List Length: {0}", token_array.Length);
 
+
 			for (int i = 0; i < token_array.Length; i++)
 			{
 				var token = token_array[i];
-				//Console.WriteLine("Check: {0}", token.Name == "newline" ? "newline" : token.Value);
-				/************* Here we lex *************
-				 * line by line, separated by newline.
-				 * 
-				 * just keep some special form as def/end/class
-				 * the rest is normal keyword.
-				 * 
-				 * special_form -> (f a b c) -> new scope ( because mostly they are new function or class ).
-				 * keyword & literal -> just  be normal sentence (c b a f) -> (f a b c) can be reversed.
-				 */
+
 				switch (token.Name)
 				{
 					case "newline":
-						// push to line stack.
-						line = acc;
-						lineStack.Push(line);
-						// clear accumulation.
+						if (acc.Count == 0) break;
+						var line = acc.ToArray();
+						blocks.Add(acc);
+						switch (line[0].Name)
+						{
+							case "special_form":
+								if (line[0].Value != "end")
+									current_special_form = line[0].Value;
+								switch (line[0].Value)
+								{
+									case "require":
+										// require a => Using a;
+										AddSource(string.Format("using {0};", line[1].Value));
+										break;
+									case "class":
+										//InspectLine(acc);
+										switch (line.Length)
+										{
+											case 2: // class A {} 
+												AddSource(string.Format("class {0} {{", line[1].Value));
+												break;
+											// class A: B
+											case 4:
+												AddSource(string.Format("class {0} : {1} {{", line[1].Value, line[3].Value));
+												break;
+
+											default:
+												// leave multi inheritance later.
+												break;
+										}
+										break;
+									case "def":
+										Console.WriteLine();
+										// def a:void b:float c:int
+										// but first we need to reverse all of them a:void -> void a
+										var pairs = new List<string>();
+										foreach (var word in line)
+										{
+											if (word.Name == "pair")
+											{
+												var pair = word.Value.Split(new char[] { ':' });
+												var new_pair = pair[1] + " " + pair[0];
+												pairs.Add(new_pair);
+												Console.Write("{0} ", new_pair);
+											}
+										}
+										// well now we got all pairs ! let add (, , , ) { 
+										var def_type_name = pairs[0]; // public void something_i_give_up_on_you
+																	  // now for the rest of them with commas:
+										var pairs_string = String.Empty;
+										if (pairs.Count > 1)
+											for (int j = 1; j < pairs.Count; j++)
+											{
+												pairs_string += pairs[j];
+												if (j != pairs.Count - 1) pairs_string += ", ";
+											}
+										// now into braces (){
+										AddSource(string.Format(
+											"public {0} ( {1} ) {{", def_type_name, pairs_string));
+										break;
+
+									case "new":
+
+										break;
+
+									case "do":
+
+										break;
+
+									case "end":
+
+										/* The idea here is pushing all nested things into a stack,
+										 * then everytimes there's an end, it pop out to match with 
+										 * topmost element, then we output each end depend on that.
+										 * 
+										 */
+										break;
+
+								}
+								break;
+							default:
+								//Console.WriteLine("Not implemented {0}", line[0]);
+								break;
+						}
+
+						// Clear Acc and new line.
 						acc = new List<Token>();
-						is_new_line = true;
-						foreach(var word in line)
-						Console.Write("{0} ", word.Value);
 						Console.WriteLine();
-					break;
-						
-					case "special_form":
-						is_special = true;
-						/* *************************************************
-						 *  We process special form here to build up things.
-						 *  Then wait for result from those small  pieces.
-						 * 
-						 ***************************************************/
-						SpecialFormProcess(token, ref is_being_nested, ref nest_level, ref line); 
-					break;
-					
+						break;
+
 					default:
-						is_special = false;
 						// accumulate them.
 						acc.Add(token);
-						//Console.Write("{0} ", token.Value);
-					break;
+						Console.Write("{0} ", token.Value);
+						break;
 				}
 			}
-		}
 
-		private void SpecialFormProcess(Token token, ref bool is_being_nested, ref int nest_level, ref List<Token> line)
-		{
-			switch (token.Value)
+
+			Console.WriteLine("\n\n[Transformed C#] \n");
+			foreach (var line in csharp_blocks)
 			{
-				case "def":
-					// Build new function block.
-					is_being_nested = true;
-					nest_level++;
-					Console.Write("_def {0}_ ", nest_level);
-					break;
-
-				case "class":
-					// Build new class block.
-					is_being_nested = true;
-					nest_level++;
-					Console.Write("_class {0}_ ", nest_level);
-					break;
-
-				case "if":
-					is_being_nested = true;
-					nest_level++;
-					Console.Write("_if {0}_ ", nest_level);
-					break;
-
-				case "require":
-					is_being_nested = false;
-					Console.Write("_require {0}_ ", nest_level);
-					break;
-
-				case "end":
-					// end every block.
-					is_being_nested = false;
-					if (nest_level <= 0) throw new Exception("lacking special form");
-					else nest_level--;
-					Console.WriteLine("__end {0}__\n", nest_level);
-					break;
+				Console.WriteLine(line);
 			}
-			//Console.WriteLine("Nested Level: {0}", nest_level);
 		}
+		private class IfBlock
+		{
+			/************************************
+			 * If 1 + 1 > 2 && true || false 
+			 *  line 1
+			 *  line 2
+			 *  line 3
+			 * end
+			 */
+			public IfBlock(List<Token> conditions, List<List<Token>> lines)
+			{
 
-		// Init all primatives.
-		public void InitImage(){
-			NewFunction ("require", (object o) => {
-				
-			});
+			}
 		}
 	}
 }
